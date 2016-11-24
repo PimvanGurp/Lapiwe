33f12f9e21aaeca735a4255e53aa9c4df7f7740c
Pim van Gurp <PimG@CURSISTZ12>
2016-11-24 11:05:02 +0100

    wip

---
+++ Lapiwe.Eventbus/src/Lapiwe.Eventbus/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lapiwe.Eventbus")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b3a5ed57-c54d-44ba-852d-2fccf7a15d34")]
