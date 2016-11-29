43a60356f9fda8fc2658e46b7ac9f02649cb695f
Lars Celie <LarsC@CURSISTZ11>
2016-11-29 11:23:34 +0100

    Eventbus: default settings changed for Lapiwe Audit: Made it work.

--- Lapiwe.Eventbus/test/Lapiwe.EventBus.IntegrationTest/TestObjects/TestDispatcherWithDuplicateParameters.cs
+++ Lapiwe.Eventbus/test/Lapiwe.EventBus.IntegrationTest/TestObjects/TestDispatcherWithDuplicateParameters.cs
@@ -1,4 +1,5 @@
 ﻿using Lapiwe.EventBus.Dispatchers;
+using Lapiwe.EventBus.Domain;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +9,11 @@ namespace Minor.WSA.EventBus.IntegrationTest
 {
     internal class TestDispatcherWithDuplicateParameters : EventDispatcher
     {
+
+        public TestDispatcherWithDuplicateParameters(BusOptions options) : base(options)
+        {
+
+        }
 
         // This class will throw an exception when constructed.
 
