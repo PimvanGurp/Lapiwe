eec18caebb36b68918ab35da7ea0aa7c34f81882
Lars Celie <LarsC@CURSISTZ11>
2016-11-29 14:00:23 +0100

    added domain entities

---
+++ Lapiwe.OnderhoudService/src/Lapiwe.OnderhoudService.Export/RegisteerOnderhoudOpdrachtCommand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lapiwe.OnderhoudService.Export
+{
+    public class RegisteerOnderhoudOpdrachtCommand
+    {
+    }
+}
